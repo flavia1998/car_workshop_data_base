70715e2a09417cd95b855c43a542fa038040d330
Flávia Pinto <flaviapinto6@gmail.com>
2025-09-16 19:02:44 +0100

    script tables triggers and data_model

---
+++ script_triggers.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE TRIGGER check_idade_funcionario
+    BEFORE INSERT ON FUNCIONARIO
+    FOR EACH ROW
+    BEGIN 
+        IF TRUNC(MONTHS_BETWEEN(SYSDATE, :NEW.DATA_NASCIMENTO) /12) < 18 THEN
+            RAISE_APPLICATION_ERROR(-20001, 'O funcionario deve ter pelo menos 18 anos.');
+        END IF;
+END check_idade_funcionario;
+/
+
+
+CREATE OR REPLACE TRIGGER check_idade_cliente
+BEFORE INSERT ON CLIENTE
+FOR EACH ROW
+BEGIN
+    
+    IF TRUNC(MONTHS_BETWEEN(SYSDATE, :NEW.DATA_NASCIMENTO)/12) < 18 THEN
+        RAISE_APPLICATION_ERROR(-20006, 'O cliente não tem idade suficiente para ser registado');
+    END IF;
+
+EXCEPTION
+   
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20007, 'Erro ao inserir cliente: ' || SQLERRM);
+END check_idade_cliente;
+/
